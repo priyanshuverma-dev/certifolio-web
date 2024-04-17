5bfdb00bbfdf62ef9923785b2751306b81f014ed
priyanshuverma-dev <priyanshuverma@outlook.in>
2024-04-17 17:18:05 +0530

    Update CertCard component for displaying certificate information

--- components/feed-section.tsx
+++ components/feed-section.tsx
@@ -1,29 +1,12 @@
 "use client";
+
 import useFeeds from "@/hooks/use-feed";
 import React from "react";
-import { AspectRatio } from "./ui/aspect-ratio";
-import Image from "next/image";
-import { Button, buttonVariants } from "./ui/button";
-import { Clipboard, ExternalLink } from "lucide-react";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { useToast } from "./ui/use-toast";
-import { MdVerified } from "react-icons/md";
 import { Skeleton } from "./ui/skeleton";
-import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import CertCard from "./main/cert-card";
 
 const FeedSection = () => {
   const { data, error, status } = useFeeds();
-  const { toast } = useToast();
-
   if (error) {
     return (
       <div className="flex items-center justify-center">
@@ -33,91 +16,41 @@ const FeedSection = () => {
   }
 
   if (status == "pending") {
-    return (
-      <div className="h-[100vh]">
-        <div className="p-2">
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        </div>
-        <div className="p-2">
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  function copyCid(cid: string) {
-    const txt = `https://ipfs.io/ipfs/${cid}`;
-    navigator.clipboard.writeText(txt);
-    toast({
-      title: "Copied to clipboard",
-    });
+    return <FeedLoading />;
   }
 
   return (
     <div className="py-2 h-full space-y-2">
       {data?.map((cert) => (
-        <div key={cert.id} className="border-2 rounded-xl p-2">
-          <div>
-            <div className="flex items-center justify-between py-2">
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight p-1">
-                {cert.title}
-              </h4>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <HiOutlineDotsCircleHorizontal className="w-7 h-7 text-muted-foreground" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Pin</DropdownMenuItem>
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Copy URL</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="text-destructive/90 font-bold hover:text-destructive focus:text-destructive">
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <img
-              className="rounded-lg w-full h-full !object-contain"
-              src={`https://gateway.pinata.cloud/ipfs/${cert.cid}`}
-              alt={`${cert.title} by ${cert.user.username}`}
-            />
-            <div className="py-2 flex flex-col">
-              <Link
-                target={cert.verifyUrl ? "_blank" : "_self"}
-                href={cert.verifyUrl ? cert.verifyUrl : "#"}
-                className="flex items-center justify-start"
-              >
-                <span className="text-sm text-muted-foreground">
-                  ISSUED BY: {cert.issuer}
-                </span>
-                {cert.verifyUrl && (
-                  <MdVerified className="ml-1 scale-80 text-green-600" />
-                )}
-              </Link>
-              <div>
-                <small className="text-sm font-medium leading-none">
-                  {cert.description}
-                </small>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CertCard cert={cert} key={cert.id} />
       ))}
     </div>
   );
 };
 
 export default FeedSection;
+
+const FeedLoading = () => {
+  return (
+    <div className="h-[100vh]">
+      <div className="p-2">
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+      </div>
+      <div className="p-2">
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/main/cert-card.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Link from "next/link";
+import { MdVerified } from "react-icons/md";
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+type Props = {
+  cert: Certificate;
+};
+
+function CertCard({ cert }: Props) {
+  return (
+    <div key={cert.id} className="border-2 rounded-xl p-2">
+      <div>
+        <div className="flex items-center justify-between py-2">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight p-1">
+            {cert.title}
+          </h4>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <HiOutlineDotsCircleHorizontal className="w-7 h-7 text-muted-foreground" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Pin</DropdownMenuItem>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuItem>Copy URL</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="text-destructive/90 font-bold hover:text-destructive focus:text-destructive">
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <img
+          className="rounded-lg w-full h-full !object-contain"
+          src={`https://gateway.pinata.cloud/ipfs/${cert.cid}`}
+          alt={`${cert.title} by ${cert.user.username}`}
+        />
+        <div className="py-2 flex flex-col">
+          <Link
+            target={cert.verifyUrl ? "_blank" : "_self"}
+            href={cert.verifyUrl ? cert.verifyUrl : "#"}
+            className="flex items-center justify-start"
+          >
+            <span className="text-sm text-muted-foreground">
+              ISSUED BY: {cert.issuer}
+            </span>
+            {cert.verifyUrl && (
+              <MdVerified className="ml-1 scale-80 text-green-600" />
+            )}
+          </Link>
+          <div>
+            <small className="text-sm font-medium leading-none">
+              {cert.description}
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CertCard;
